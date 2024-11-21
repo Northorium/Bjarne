26c29172cf9c1124fd5888c2fed34d7fe931c309
Northorium <63539373+Northorium@users.noreply.github.com>
2024-11-21 14:58:54 +0100

    Initial commit

---
+++ Bjarne/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bjarne
+{
+    internal class Person
+    {
+        string name;
+        int age;
+        string hobbies;
+
+
+        public Person(string personName, int personAge, string personHobbies)
+        {
+            name = personName;
+            age = personAge;
+            hobbies = personHobbies;
+        }
+
+        public void printText()
+        {
+            Console.WriteLine($"Jeg heter {name}, og jeg er {age} ung. På fritiden liker jeg å {hobbies}.");
+        }
+    }
+}
---
+++ Bjarne/Program.cs
@@ -0,0 +1,28 @@
+﻿//) Model yourself
+// 
+// I denne oppgaven skal du prøve å programmere deg selv 🤓 Du har noen gitte egenskaper og ting du liker å gjøre. Lag et hovedprogram der hver av deltakerene har sin klasse som kan printe ut en introduksjonstekst, og også utføre hver sine hobbyer.
+// 
+// Utskrift i konsollen kan feks se sånn her ut: */
+
+//Velge person
+//Be om navn
+//Be om alder
+
+using Bjarne;
+
+Console.WriteLine("Hva heter du?");
+var name = Console.ReadLine();
+
+
+
+Console.WriteLine("Hvor gammel er du?");
+var age = Console.ReadLine();
+int ageToInt = Convert.ToInt32(age);
+
+
+Console.WriteLine("Hva liker du å gjøre på fritiden?");
+var hobbies = Console.ReadLine();
+
+Person Kim = new Person(name, ageToInt, hobbies);
+
+Kim.printText();
